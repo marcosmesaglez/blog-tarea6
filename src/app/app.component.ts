9148fe62235f6b2c138812021069a9cd37db9b56
Marcos Mesa <marcos.mesa730@comunidadunir.net>
2025-03-22 11:46:53 +0100

    Creación navegación + corrección errores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'blog-tarea6';
+  title = 'angular-project-master-web';
 }
